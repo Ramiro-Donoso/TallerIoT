7edd6c3760097cb35616f259adf697625f4f1e90
Ramiro-Donoso <ramiro.donoso@gmail.com>
2021-09-09 13:44:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,7 @@
 #include <stdio.h>
-#include "lib/ledStruct.h"
-#include "lib/ledRgbStruct.h"
 #include "lib/system.h"
-#include "lib/pinStruct.h"
-#include "lib/buzzerStruct.h"
-#include "lib/servoStruct.h"
-#include "lib/therm.h"
 
 int main () {
-	
-Therm t = Therm();	
-	/*
-	Servo s = Servo(24);
-	s.setSpeed(100);
-	s.moveTo(0);
-	s.moveTo(180);
-	*/
-	/*
-	int x =3;
-	float y=5.6;
-	char c[20]={"hola"};
-	print(c);
-	print('\n');
-	x=scan(c,2r0);
-	print(c);
-	print('\n');
-	print(x);
-	print('\n');
-	print((int)(c[x]));
-	*/
+	print("Hola Mundo!!");
 	return 0;
 }
